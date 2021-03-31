fd8a0b020c5015883a9bdc771358f17293600145
Daniel <dordan0401@gmail.com>
2021-03-31 14:12:11 -0400

    Add project files.

---
+++ HypeTracker.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(HypeTracker.WebMVC.Startup))]
+namespace HypeTracker.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
